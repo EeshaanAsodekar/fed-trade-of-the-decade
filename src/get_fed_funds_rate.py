a3e2fff0a36f1430e538fce128c94ca89fa34682
EeshaanAsodekar <epa6822@nyu.edu>
2024-12-13 18:11:04 -0500

    removed the fomc dates snippet

--- src/get_fed_funds_rate.py
+++ src/get_fed_funds_rate.py
@@ -64,32 +64,3 @@ fed_funds_df = pd.DataFrame(fed_funds_target_rate)
 fed_funds_df.to_csv("data/raw/fed_funds_target_rate.csv", index=False)
 
 print(fed_funds_df.head())
-
-
-
-
-### get fomc meeting dates
-# NOTE: for any meeting date e.g. 2024-11-07 the rate change is effective from next
-# day viz. 2024-11-08
-import os
-import pandas as pd
-
-# Directory containing FOMC statements
-statements_dir = "data/raw/FOMC/statements"
-
-# List all .txt files in the directory
-statement_files = [f for f in os.listdir(statements_dir) if f.endswith(".txt")]
-
-# Extract meeting dates from filenames
-fomc_data = []
-for filename in statement_files:
-    # Extract date from the filename (assumes format YYYY-MM-DD_Statements.txt)
-    meeting_date = filename.split("_")[0]
-    fomc_data.append({"fomc_meeting_date": meeting_date})
-
-# Create a DataFrame from the extracted dates
-fomc_dates_df = pd.DataFrame(fomc_data)
-fomc_dates_df.to_csv("data/raw/fomc_meeting_dates.csv")
-# Display the DataFrame
-print(fomc_dates_df.head())
-print(fomc_dates_df.tail())
